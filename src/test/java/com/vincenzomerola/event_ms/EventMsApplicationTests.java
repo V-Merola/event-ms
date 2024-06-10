9c9c9907013d84a661af6f18ca696bc3387d3ee8
V-Merola <vincenzo.merola18@gmail.com>
2024-06-10 18:10:29 +0200

    Initial commit

---
+++ src/test/java/com/vincenzomerola/event_ms/EventMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincenzomerola.event_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
